de45a7a3dfa7b5b1099991829a1da6eb3fba2689
braedencallaghan <115768702+braedencallaghan@users.noreply.github.com>
2023-11-11 21:21:46 -0500

    Update Home.js

--- react-front-end/src/pages/Home.js
+++ react-front-end/src/pages/Home.js
@@ -3,25 +3,27 @@ import React, { useState, useEffect, useRef } from 'react';
 function VideoStream() {
   const videoRef = useRef(null);
 
-    useEffect(() => {
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(stream => {
-                    let video = videoRef.current;
-                    video.srcObject = stream;
-                    video.play();
-                })
-                .catch(err => {
-                    console.error("Error accessing the webcam: ", err);
-                });
+  useEffect(() => {
+    async function getMedia() {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          let video = videoRef.current;
+          video.srcObject = stream;
+          await video.play();
+        } catch (err) {
+          console.error("Error accessing the webcam: ", err);
         }
-    }, []);
+      }
+    }
+    getMedia();
+  }, []);
 
-    return (
-        <div>
-            <video ref={videoRef} />
-        </div>
-    );
+  return (
+    <div>
+      <video ref={videoRef}/>
+    </div>
+  );
 }
 
 
