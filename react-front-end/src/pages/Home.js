82aca437f468758218f90f477c33f838b6d56a9c
Braeden Callaghan <bcallaghan@umass.edu>
2023-11-12 04:13:01 -0500

    Fixed all merge conflicts

--- react-front-end/src/pages/Home.js
+++ react-front-end/src/pages/Home.js
@@ -2,44 +2,6 @@ import React, { useState } from 'react';
 import WebcamComponent from '../components/WebcamComponent';
 import styles from '../styles/componentStyles.module.css';
 
-const Home = () => {
-  const [capturedImage, setCapturedImage] = useState(null);
-
-  const handleCapture = (imageSrc) => {
-    setCapturedImage(imageSrc);
-  };
-
-  const handleSendToServer = () => {
-    fetch('http://localhost:3000/process-image', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ image: capturedImage }),
-    })
-      .then(response => response.json())
-    .then(response => {
-      // Handle the response from the server
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error('Error sending image to server:', error);
-    });
-  };
-
-  return (
-    <div>
-      <WebcamComponent onCapture={handleCapture} />
-      {capturedImage && (
-        <div>
-          <img src={capturedImage} alt="Captured" />
-          <button onClick={handleSendToServer}>Send to Server</button>
-        </div>
-      )}
-    </div>
-  );
-}
-
 function Counter() {
     const [count, setCount] = useState(0);
 
@@ -61,14 +23,37 @@ function Counter() {
     );
 }
 
-function Home() {
+
+function Home(){
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const handleCapture = (imageSrc) => {
+    setCapturedImage(imageSrc);
+  };
+
+  async function handleSendToServer() {
+    const res = await fetch('http://localhost:3000/process-image', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ image: capturedImage }),
+    })
+  };
+
   return (
     <div>
       <h1 className={styles['centered-heading']}>PushUp ShowDown</h1>
-      <div className={styles['video-container']}><VideoStream /></div>
+      <WebcamComponent onCapture={handleCapture} />
+      {capturedImage && (
+        <div>
+          <img src={capturedImage} alt="Captured" />
+          <button onClick={handleSendToServer}>Send to Server</button>
+        </div>
+      )}
       <div className={styles['centered-element']}><Counter /></div>
     </div>
-  )
+  );
 }
 
 export default Home;
