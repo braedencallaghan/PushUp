77f2b73fd809034cd5e5970e6847b7ff8eebc14f
Braeden Callaghan <bcallaghan@umass.edu>
2023-11-12 04:15:52 -0500

    Fixed backend merge conflicts

--- server/index.js
+++ server/index.js
@@ -1,28 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-<<<<<<< HEAD
-import {MongoClient} from 'mongodb';
-import imageRouter from './routes/imageRouter.js';
-import bodyParser from 'body-parser';
-=======
 import router from './routes/routes.js';
->>>>>>> main
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-<<<<<<< HEAD
-app.use(bodyParser.json());
 
-const imageRouter = imageRouter();
-
-app.use('/api/users', imageRouter);
-=======
 app.use(router);
->>>>>>> main
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
