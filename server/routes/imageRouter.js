59ab59d17e69b3c5439364bd2bb3021a666ae9c5
Lorenzo Inglese <113322852+linglese@users.noreply.github.com>
2023-11-11 21:33:55 -0500

    Database Connection

---
+++ server/routes/imageRouter.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const imageRouter = express.Router();
+
+imageRouter.post('/', async (req, res) => {
+    // Handle POST request to /api/users here
+    });
+
+module.exports = imageRouter;
+
+
+
